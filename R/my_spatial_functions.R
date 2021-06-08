2bfdc5380df3bcb8a5ea7f90cbf7495be8e4e14c
HemingNM <neanderh@yahoo.com.br>
2021-06-08 18:31:58 -0300

    First commit

---
+++ R/my_spatial_functions.R
@@ -0,0 +1,118 @@
+
+## My spatial functions
+
+#' Convert distance from km to decimal degrees
+#' 
+#' @param dist Numeric. Vector of distances to be converted from km to lat degrees
+#' @param y Numeric or Extent object, or any object from which an 
+#' Extent object can be extracted. See ?raster::crop for details
+#' @export
+km2decdeg <- function(dist, y){
+  if(is.numeric(y)){
+    dist*(cos(y*(pi/180)))/111.1194
+  } else {
+    dist*(cos(mean(c(raster::ymin(y), raster::ymax(y)))*(pi/180)))/111.1194 
+  }
+}
+
+##
+crs.m <- function(x){
+  raster::crs(
+    paste0("+proj=eqdc +units=km", # eqdc \ laea
+           " +lat_1=",
+           mean(raster::ymin(x)),
+           " +lat_2=",
+           mean(raster::ymax(x)),
+           " +lon_0=",
+           mean(c(raster::xmax(x), raster::xmin(x))) )
+  )
+}
+
+
+#' Compute beta diversity on arrays
+#' 
+#' This function will compute beta diversity on arrays where 
+#' the first two dimensions are sites and the third dimension is species
+#' 
+#' @param x numeric array of with 3 dimensions
+#' @param fwCC vector of observations to be kept. It is a matrix returned
+#' from raster::focalWeight and coerced to vector with as.vector()
+#' @inheritParams betapart::phylo.beta.pair
+#' @inheritParams betapart::beta.pair
+#' @export
+beta.array <- function(x, index.family="sorensen", tree=F, fwCC){
+  # print(x)
+  x <- apply(x, 3, FUN=as.vector)
+  x <- x[stats::complete.cases(x) & fwCC,]
+  
+  mean_turnover <- mean_nestedness <- mean_beta <- numeric(1)
+  
+  if(all(is.na(x))) {
+    mean_turnover <- mean_nestedness <- mean_beta <- 0
+  } else if(sum(x)==0) {
+    return(c(mean_turnover, mean_nestedness, mean_beta))
+  } else {
+    if(tree==F){
+      res <- betapart::beta.pair(x, index.family=index.family)
+    } else {
+      res <- betapart::phylo.beta.pair(x, tree, index.family=index.family)
+    }
+    mean_turnover <- mean(as.matrix(res[[1]])[2:length(as.matrix(res[[1]])[,1]),1], na.rm=TRUE) 
+    mean_nestedness <- mean(as.matrix(res[[2]])[2:length(as.matrix(res[[2]])[,1]),1], na.rm=TRUE)
+    mean_beta <- mean(as.matrix(res[[3]])[2:length(as.matrix(res[[3]])[,1]),1], na.rm=TRUE)
+  }
+  return(c(mean_turnover=mean_turnover, 
+           mean_nestedness=mean_nestedness, 
+           mean_beta=mean_beta))
+}
+
+
+
+#' Compute beta diversity on raster bricks or stacks
+#' 
+#' This function will compute beta diversity on raster bricks or stacks
+#' that contain binary (presence/absence) species distribution data
+#' 
+#' @param x raster brick or raster stack of species presence/absence
+#' @param radius window radius to compute beta diversity metrics
+#' @param numCores Number of cores to be used in parallel calculation
+#' @param overwrite Logical. Should saved files be overwritten with new values?
+#' @inheritParams betapart::phylo.beta.pair
+#' @inheritParams betapart::beta.pair
+#' @inheritParams raster::focalWeight
+#' @inheritParams raster::writeRaster
+#' @export
+beta.stack <- function(x, radius = 2.8, type = "circle",  
+                       index.family="sorensen", tree=F,
+                       format="GTiff", filename=NULL, overwrite=T,
+                       numCores=1) {
+  
+  fw <- raster::focalWeight(x, km2decdeg(radius, x), type=type)
+  fw[fw>0] <- 1
+  fw[fw==0] <- NA
+  fwCC <- stats::complete.cases(as.vector(fw))
+  
+  # run
+  if(numCores>1){
+    spatial.tools::sfQuickInit(numCores)
+  }
+  # spatial.tools::sfQuickInit()
+  betaR <- spatial.tools::rasterEngine(x=x,
+                                       args=list(index.family=index.family, tree=tree, fwCC=fwCC), 
+                                       fun=beta.array, 
+                                       window_dims=dim(fw),
+                                       outbands=3,
+                                       setMinMax=T,
+                                       .packages="betapart")
+  if(numCores>1){
+    spatial.tools::sfQuickStop()
+  }
+  
+  names(betaR) <- c("turnover", "nestedness", "beta")
+  if(!is.null(filename)){
+    betaR <- raster::writeRaster(betaR, filename = filename,
+                         overwrite=overwrite, format=format)
+  }
+  return(betaR)
+}
+# end
---
+++ documenting/documentignore.R
@@ -0,0 +1,54 @@
+# install.packages("devtools")
+library(devtools)
+# devtools::use_testthat()
+# devtools::use_vignette("introduction")
+## Wickham 2015 R packages (pg 98) Documenting package
+# ?devtools::use_package()
+
+### First run
+# https://happygitwithr.com/rstudio-git-github.html
+# https://community.rstudio.com/t/procedure-to-create-a-project-for-a-package-destined-for-github/2054/4
+usethis::create_package(getwd())
+# devtools::create("nbThin")
+# devtools::build(getwd())
+
+### Configuring github
+usethis::git_sitrep()
+usethis::git_vaccinate()
+credentials::set_github_pat("7354617e689df6ee2e5a39ca4a5f92b221c15ac4")
+
+usethis::use_build_ignore(c("documenting", "parallel.md"))
+devtools::document()
+devtools::check()
+# devtools::load_all()
+# roxygen2::roxygenise()
+
+devtools::install_github("HemingNM/ENMwizard")
+
+# for building windows package
+devtools::build_win()
+path <- "../ENMwizard_src"
+if(dir.exists(path)==F){dir.create(path)}
+devtools::build(path=path)
+
+
+
+
+# # general function documentation
+#
+# #' Function Title (short description)
+# #'
+# #' General function description. A short paragraph (or more) describing what the function does.
+# #' @param arg1 List of occurence data. See argument "occ_locs" in mxnt.cp.
+# #' @inheritParams function1
+# #' @return objects returned from function
+# #' @examples
+# #' plot(mxnt.mdls.preds.lst[[1]][[4]]) # MaxEnt predictions, based on the model selection criteria
+# #' @export
+#
+
+
+# Bvarieg.occ <- read.table(paste(system.file(package="dismo"), "/ex/bradypus.csv", sep=""), header=TRUE, sep=",")
+# colnames(Bvarieg.occ) <- c("SPEC", "LONG", "LAT")
+# spp.occ.list <- list(Bvarieg = Bvarieg.occ)
+# occ_polys <- f.poly.batch(spp.occ.list, o.path="occ_poly")
