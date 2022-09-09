83ce5cc17e31525250b0dc9a62edfb546df622cb
Neander Marcel Heming <neanderh@yahoo.com.br>
2022-09-09 16:16:19 -0300

    updated beta.vec and beta.spat for terra compatibility: w matrix dimensions must be odd. See ?terra::focal3D

--- R/beta_spatial.R
+++ R/beta_spatial.R
@@ -12,20 +12,25 @@
 #' @inheritParams betapart::beta.pair
 # #' @export
 beta.vec <- function(x, nspp, index.family="sorensen", tree=NA){
+  # print(nspp)
   x <- matrix(x, ncol=nspp, byrow = F)
-  tofill <- apply(x, 1, function(x){sum(is.na(x))}) #rowSums(x) < nspp
-  tofill <- tofill > 0 & tofill < nspp
-  x[tofill, is.na(x[tofill,])] <- 0
-  x <- x[stats::complete.cases(x),] # maybe replace NAs with 0
+  # print(x)
+  # tofill <- apply(x, 1, function(x){sum(is.na(x))}) #rowSums(x) < nspp
+  
+  # xna <- is.na(x)
+  # tofill <- rowSums(xna)
+  # tofill <- tofill > 0 & tofill < nspp
+  # x[tofill, is.na(x[tofill,])] <- 0
+  x <- x[stats::complete.cases(x) & rowSums(x, na.rm = T)>0,] # maybe replace NAs with 0
   
   mean_turnover <- mean_nestedness <- mean_beta <- numeric(1)
   
   if(all(is.na(x))){
-    return(c(NA, NA, NA, NA))
+    return(c(turnover=NA, nestedness=NA, beta=NA, beta_ratio=NA))
   } else if(!inherits(x, "matrix")) {
-    return(c(mean_turnover, mean_nestedness, mean_beta, mean_nestedness/mean_beta))
+    return(c(turnover=mean_turnover, nestedness=mean_nestedness, beta=mean_beta, beta_ratio=mean_nestedness/mean_beta))
   } else if(sum(x, na.rm = T)==0) {
-    return(c(mean_turnover, mean_nestedness, mean_beta, mean_nestedness/mean_beta))
+    return(c(turnover=mean_turnover, nestedness=mean_nestedness, beta=mean_beta, beta_ratio=mean_nestedness/mean_beta))
   } else {
     if(is.na(tree)){
       res <- betapart::beta.pair(x, index.family=index.family)
@@ -45,9 +50,12 @@ beta.vec <- function(x, nspp, index.family="sorensen", tree=NA){
     if(sum(res[[3]], na.rm = T)==0){
       mean_beta <- 0     
     } else {
+      # print(x)
+      # print(res[[3]])
       mean_beta <- mean(res[[3]][lower.tri(res[[3]])], na.rm=T) # mean(as.matrix(res[[3]])[2:length(as.matrix(res[[3]])[,1]),1], na.rm=TRUE)
     }
-    return(c(mean_turnover, mean_nestedness, mean_beta, mean_nestedness/mean_beta))
+    return(c(turnover=mean_turnover, nestedness=mean_nestedness, beta=mean_beta, beta_ratio=mean_nestedness/mean_beta))
+    # return(c(mean_turnover, mean_nestedness, mean_beta, mean_nestedness/mean_beta))
   }
 }
 
@@ -77,6 +85,9 @@ beta.spat <- function(x, fm=NULL, d = mean(terra::res(terra::rast(x)))*2, type =
   if(!inherits(x, c("SpatRaster"))){
     x <- terra::rast(x)
   }
+  
+  # nspp <- terra::nlyr(x)
+  
   if(is.null(fm)){
     min.d <- sqrt(prod(terra::res(x))) #mean(res(x)*112)
     if(d < min.d) {
@@ -86,19 +97,47 @@ beta.spat <- function(x, fm=NULL, d = mean(terra::res(terra::rast(x)))*2, type =
     
     fm <- terra::focalMat(x, d, 
                           type = type,
-                          fillNA = T)
+                          fillNA = F)
   }
   # transformar tudo em 1 para encontrar valores exatos
   fm[] <- fm/max(fm, na.rm = T)
-  # create array to 3D focal calculations
-  fmA <- replicate(terra::nlyr(x), fm)
+  
+  ## test even-odd dimensions in window
+  # 'terra::focal3D' only works with odd dimensions
+  even <- (c(dim(fm), terra::nlyr(x)) %% 2) == 0
+  if(any(even)){
+    # test if fm dims are even
+    if(even[1]){
+      fm <- rbind(fm, 0)
+    }
+    if(even[2]){
+      fm <- cbind(fm, 0)
+    }
+    # test if number of spp layers is even
+    if(even[3]){
+      # add layer to get odd dimensions
+      x <- c(x, rast(x, nlyrs=1, vals=0))
+      # create array to 3D focal calculations
+      fmA <- replicate(terra::nlyr(x), fm)
+      # set weight 0 for last layer
+      fmA[,,terra::nlyr(x)] <- 0
+    } else {
+      # create array to 3D focal calculations
+      fmA <- replicate(terra::nlyr(x), fm)
+    }
+  } else {
+    # create array to 3D focal calculations
+    fmA <- replicate(terra::nlyr(x), fm)
+  }
+  
+  # print(terra::nlyr(x))
   
   betaR <- terra::focal3D(x, fmA,
                           beta.vec,
                           index.family=index.family, 
-                          tree=tree, nspp=terra::nlyr(x),
-                          na.policy="all")[[1:4]]
-  names(betaR) <- c("turnover", "nestedness", "beta_div", "beta_ratio")
+                          tree=tree, nspp=terra::nlyr(x), # nspp=nspp
+                          na.policy="all")#[[1:4]]
+  # names(betaR) <- c("turnover", "nestedness", "beta_div", "beta_ratio")
   
   if(!is.null(filename)){
     betaR <- terra::writeRaster(betaR, filename = filename,
